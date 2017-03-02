9b7fcc924bbcf29a40b96ff269f76539bbdb85a6
Andrés Moncada Jiménez <pimamav2017@gmail.com>
2017-03-02 16:59:01 +0100

    Bluetooth comprobacion y error

--- app/src/main/java/com/example/ur/mava_cr/MainActivity.java
+++ app/src/main/java/com/example/ur/mava_cr/MainActivity.java
@@ -3,9 +3,11 @@ package com.example.ur.mava_cr;
 import android.bluetooth.BluetoothAdapter;
 
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     static final int REQUEST_ENABLE_BT = 1;
@@ -27,15 +29,28 @@ public class MainActivity extends AppCompatActivity {
 
         }
         else {
+            Context context = getApplicationContext();
+            CharSequence text = "tu movil no soporta bluetooth";
+            int duration = Toast.LENGTH_SHORT;
+
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+
             finish();
         }
     }
     protected void onActivityForResult (int requestcode, int resultcode, Intent enabledItent){
         if (requestcode == REQUEST_ENABLE_BT){
             if(resultcode == RESULT_OK){
-                
+                //nothing
             }else{
+                Context context = getApplicationContext();
+                CharSequence text = "bluetooth no activado";
+                int duration = Toast.LENGTH_SHORT;
 
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+                finish();
             }
 
         }
