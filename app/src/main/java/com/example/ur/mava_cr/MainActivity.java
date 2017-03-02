7598d46417a638d707ba1615bc016d55d38c7a79
Andrés Moncada Jiménez <pimamav2017@gmail.com>
2017-03-02 16:48:07 +0100

    Bluetooth comprobacion

--- app/src/main/java/com/example/ur/mava_cr/MainActivity.java
+++ app/src/main/java/com/example/ur/mava_cr/MainActivity.java
@@ -8,22 +8,34 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    static final int REQUEST_ENABLE_BT = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
     public void mostrarDispositivos () {
-        final int REQUEST_ENABLE_BT = 1;
+
         BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-        if (mBluetoothAdapter == null) {
+        if (mBluetoothAdapter != null) {
             // Device does not support Bluetooth
+
             if (!mBluetoothAdapter.isEnabled()) {
                 Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
                 startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+            }
 
-                //onActivityResult(REQUEST_ENABLE_BT,resultCode,enableBtIntent);
+        }
+        else {
+            finish();
+        }
+    }
+    protected void onActivityForResult (int requestcode, int resultcode, Intent enabledItent){
+        if (requestcode == REQUEST_ENABLE_BT){
+            if(resultcode == RESULT_OK){
+                
+            }else{
+
             }
 
         }
