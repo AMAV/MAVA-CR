bf7dfd0be68f001d87093bd09b717b1f0e0ccc16
Andrés Moncada Jiménez <pimamav2017@gmail.com>
2017-03-02 16:29:54 +0100

    Bluetooth start

--- app/src/main/java/com/example/ur/mava_cr/MainActivity.java
+++ app/src/main/java/com/example/ur/mava_cr/MainActivity.java
@@ -1,5 +1,9 @@
 package com.example.ur.mava_cr;
 
+import android.bluetooth.BluetoothAdapter;
+
+
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +13,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void mostrarDispositivos () {
+        final int REQUEST_ENABLE_BT = 1;
+        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (mBluetoothAdapter == null) {
+            // Device does not support Bluetooth
+            if (!mBluetoothAdapter.isEnabled()) {
+                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+
+                //onActivityResult(REQUEST_ENABLE_BT,resultCode,enableBtIntent);
+            }
+
+        }
     }
 }
