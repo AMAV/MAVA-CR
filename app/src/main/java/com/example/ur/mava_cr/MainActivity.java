299d2ebdc544a0cdbd7162834ae061c11b5fb7e0
andres <pimamav2017@gmail.com>
2017-03-07 17:02:05 +0100

    Correccion del crash

--- app/src/main/java/com/example/ur/mava_cr/MainActivity.java
+++ app/src/main/java/com/example/ur/mava_cr/MainActivity.java
@@ -7,6 +7,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    public void mostrarDispositivos () {
+    public void mostrarDispositivos (View view) {
 
         BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         if (mBluetoothAdapter != null) {
